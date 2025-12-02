b6eb8c4decb2f219cf9e9d5d74514284f3154aad
Johnny Yang <johnnyyang@google.com>
2025-12-02 09:42:29 -0800

    set up release branch for v0.12.0
    Signed-off-by: Johnny Yang <johnnyyang@google.com>

--- .buildkite/scripts/setup_docker_env.sh
+++ .buildkite/scripts/setup_docker_env.sh
@@ -59,7 +59,8 @@ setup_environment() {
   python3 -m pip install --progress-bar off buildkite-test-collector==0.1.9
   echo "Python dependencies installed"
 
-  VLLM_COMMIT_HASH=$(buildkite-agent meta-data get "VLLM_COMMIT_HASH" --default "")
+#  VLLM_COMMIT_HASH=$(buildkite-agent meta-data get "VLLM_COMMIT_HASH" --default "")
+  VLLM_COMMIT_HASH="v0.12.0"
 
   docker build \
       --build-arg VLLM_COMMIT_HASH="${VLLM_COMMIT_HASH}" \
